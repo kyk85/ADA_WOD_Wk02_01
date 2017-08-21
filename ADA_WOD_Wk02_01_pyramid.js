5b29e8d395ebfb029e817353c96db160169b6537
KYK <kay2kay1@gmail.com>
2017-08-21 22:34:27 +0800

    First Commit

---
+++ ADA_WOD_Wk02_01_pyramid.js
@@ -0,0 +1,12 @@
+var triBase=20
+var baseCount=''
+
+function drawTriangle(){
+	for (i=0; i<triBase; i++){
+		document.write(baseCount += '&num;');
+		document.write("<br/>");
+	}
+}
+
+drawTriangle()
+	
